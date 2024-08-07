b653b442090a7c30cda0abafcc63a82110c58e1c
Renato Castro <rspindola@live.com>
2024-08-07 20:41:55 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,17 @@
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -14,15 +21,44 @@ const SidebarSheet = () => {
       </SheetHeader>
 
       {/* USER */}
-      <div className="flex items-center gap-3 border-b border-solid py-2">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-2">
+        <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+
+        <Dialog>
+          <DialogTrigger>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-1 font-bold" asChild>
+              <Image
+                src="/google.svg"
+                width={18}
+                height={18}
+                alt="Fazer login com o Google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage src="/avatar.jpg" alt="Usuário" />
         </Avatar>
 
         <div>
           <p className="font-bold">Ghabriel Molina</p>
           <p className="text-xs">ghabrielmolina@hotmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       {/* START BUTTON AND BOOKINGS BUTTON */}
