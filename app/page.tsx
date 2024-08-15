2e27b57c2cdcf233a54ebb99bd050c01146c5126
Renato Castro <rspindola@live.com>
2024-08-15 20:55:07 -0300

    feat: ensure booking item takes full width on home page

--- app/page.tsx
+++ app/page.tsx
@@ -103,13 +103,13 @@ const Home = async () => {
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
               Agendamentos
             </h2>
+            <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+              {confirmedBookings.map((booking) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
+            </div>
           </>
         )}
-        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {confirmedBookings.map((booking) => (
-            <BookingItem key={booking.id} booking={booking} />
-          ))}
-        </div>
 
         {/* RECOMMENDED FIELD */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
